933b57f23e1e54fa80cfe405336e7f981b346f1f
Name <user@example.com>
2021-03-15 08:14:48 -0400

    rename min_absolute to min_gb, use parameter names

--- all_checks.py
+++ all_checks.py
@@ -3,20 +3,26 @@
 import shutil
 import sys
 
-def check_disk_full(disk, min_absolute, min_percent):
+def check_reboot()
+    return os.path.exists("/run/reboot-required")
+
+def check_disk_full(disk, min_gb, min_percent):
     """Returns True if there is enough free disl space, false otherwise."""
     du = shutil.disk_usage(disk)
     #Calculatethe percentage of free space
     percent_free = 100 * du.free /du.total
     #Calculatethe how many free gigabytes
     gigabytes_free = du.free /2**30
-    if percent_free < min_percent or gigabytes_free < min_absolute:
+    if percent_free < min_percent or gigabytes_free < min_gb:
         return False
     return True
 
 #Check for at least 2 GB and 10% free
-if not check_disk_usage("/", 2, 10):
-    print("ERROR: Not enough disk space.")
+if  check_reboot():
+    print("Pending Reboot")
+    sys.exit(1)
+if  check_disk_full(disk="/", min_gb=2, min_percent=10):
+    print("Disk full")
     sys.exit(1)
 
 print("Everything ok.")
